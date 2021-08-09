f6deca8bf0dcd9952d082a7d1387ec11495cf4cf
Nelson Foltz <nelsfoltz@gmail.com>
2021-08-09 10:03:12 -0400

    add: basic folder scaffold

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f6772b6e80f69ceb5b54";
+/******/ 	var hotCurrentHash = "af43634168583309c0f9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
